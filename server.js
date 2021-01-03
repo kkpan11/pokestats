b12a12f220dda739948028a502697caae1a00c29
Andre Ferreira <43468798+andreferreiradlw@users.noreply.github.com>
2021-01-03 12:04:45 +0000

    Custom Server (#21)
    * Creates server.js and installs dependencies

---
+++ server.js
@@ -0,0 +1,25 @@
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+const PORT = process.env.PORT || 3000
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    // Be sure to pass `true` as the second argument to `url.parse`.
+    // This tells it to parse the query portion of the URL.
+    const parsedUrl = parse(req.url, true)
+    const { pathname, query } = parsedUrl
+    if (pathname === '/home') {
+      app.render(req, res, '/', query)
+    } else {
+      handle(req, res, parsedUrl)
+    }
+  }).listen(PORT, err => {
+    if (err) throw err
+    console.log(`> Server Started on ${PORT}`)
+  })
+})
