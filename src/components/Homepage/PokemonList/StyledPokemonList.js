6b25591e48c5d0c9309c99562468919eeba3e379
Andre Ferreira <43468798+andreferreiradlw@users.noreply.github.com>
2021-08-30 13:12:52 +0100

    Enable Sorting Pokemon in Homepage (#72)
    * Sort Pokemon List  Functionality
    
    * Responsive Styles For Select Elements

--- src/components/Homepage/PokemonList/StyledPokemonList.js
+++ src/components/Homepage/PokemonList/StyledPokemonList.js
@@ -9,10 +9,16 @@ const Container = styled(Box)`
 `
 
 const SelectContainer = styled(Box)`
-  margin-bottom: 1rem;
+  div {
+    margin-bottom: 1.5rem;
 
-  & label {
-    margin-right: 0.5rem;
+    label {
+      margin-right: 0.5rem;
+    }
+  }
+
+  div:nth-of-type(1) {
+    margin-right: 1.5rem;
   }
 `
 
--- src/components/Homepage/PokemonList/index.js
+++ src/components/Homepage/PokemonList/index.js
@@ -14,8 +14,21 @@ export default function PokemonList() {
   const pokemon = useSelector(state => state.home.pokemon)
   // display pokemon list
   const [showPokemon, setShowPokemon] = useState([])
-  // select state
+  // gen select state
   const [gen, setGen] = useState('all')
+  // sort select state
+  const [sortBy, setSortBy] = useState('id')
+
+  const sortPokemon = (pokemonList, sortBy) =>
+    [...pokemonList].sort((a, b) => {
+      if (a[sortBy] > b[sortBy]) return 1
+      if (a[sortBy] < b[sortBy]) return -1
+      return 0
+    })
+
+  useEffect(() => {
+    showPokemon?.length && setShowPokemon(sortPokemon(showPokemon, sortBy))
+  }, [sortBy])
 
   useEffect(() => {
     setShowPokemon([])
@@ -24,8 +37,10 @@ export default function PokemonList() {
       const filteredPokemon = pokemon.filter(
         pokemon => gen === mapIdToGeneration(pokemon.id)
       )
+      // sort
+      const sortedPokemon = sortPokemon(filteredPokemon, sortBy)
       // show state
-      setShowPokemon(filteredPokemon)
+      setShowPokemon(sortedPokemon)
     } else {
       setShowPokemon(pokemon)
     }
@@ -43,24 +58,45 @@ export default function PokemonList() {
             justify="flex-start"
           >
             <SectionTitle>{`Select your Pokemon (${showPokemon.length})`}</SectionTitle>
-            <SelectContainer direction="row" justify="flex-start">
-              <label id="generation" htmlFor="gen_select">
-                Game Generation:
-              </label>
-              <Select
-                light
-                aria-labelledby="generation"
-                id="gen_select"
-                value={gen}
-                onChange={e => setGen(e.target.value)}
-              >
-                <option value="all">All</option>
-                {generations.map(({ genDescription, genValue }, i) => (
-                  <option key={`${genValue}-${i}`} value={genValue}>
-                    {genDescription}
-                  </option>
-                ))}
-              </Select>
+            <SelectContainer
+              direction="row"
+              justify={{ xxs: 'center', sm: 'flex-start' }}
+              flexWrap="wrap"
+            >
+              <Box direction="row" justify="flex-start" width="auto">
+                <label id="generation" htmlFor="gen_select">
+                  Game Generation:
+                </label>
+                <Select
+                  light
+                  aria-labelledby="generation"
+                  id="gen_select"
+                  value={gen}
+                  onChange={e => setGen(e.target.value)}
+                >
+                  <option value="all">All</option>
+                  {generations.map(({ genDescription, genValue }, i) => (
+                    <option key={`${genValue}-${i}`} value={genValue}>
+                      {genDescription}
+                    </option>
+                  ))}
+                </Select>
+              </Box>
+              <Box direction="row" justify="flex-start" width="auto">
+                <label id="sorting" htmlFor="sort_pokemon">
+                  Sort Pokemon:
+                </label>
+                <Select
+                  light
+                  aria-labelledby="sorting"
+                  id="sort_pokemon"
+                  value={sortBy}
+                  onChange={e => setSortBy(e.target.value)}
+                >
+                  <option value="id">Number</option>
+                  <option value="name">Name</option>
+                </Select>
+              </Box>
             </SelectContainer>
             {showPokemon.length > 0 && (
               <InfiniteScroll sizes={12} pokemonList={showPokemon} />
