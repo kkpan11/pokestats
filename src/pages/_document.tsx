77865708e2b0ed1d2969c4f73735389a2a1ee3ed
Andre Ferreira <43468798+andreferreiradlw@users.noreply.github.com>
2023-02-28 23:05:35 +0000

    Changes Font Display to Swap (#132)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -34,7 +34,7 @@ export default class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,700;1,700&family=Montserrat:wght@400;500;600;700&family=Quicksand:wght@600;700&display=block"
+            href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,700;1,700&family=Montserrat:wght@400;500;600;700&family=Quicksand:wght@600;700&display=swap"
             rel="stylesheet"
             id="generate-googlefonts-css"
             crossOrigin="true"
