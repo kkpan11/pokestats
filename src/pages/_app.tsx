c05c1c060b9ac69d64aa7ac2b2ff3f3151dfa766
Andre Ferreira <43468798+andreferreiradlw@users.noreply.github.com>
2024-08-05 12:05:42 +0100

    Implements Tanstack Query (#150)
    * installs tanstack query packages
    
    * implements query client in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 // analytics
 import PlausibleProvider from 'next-plausible';
 // types
@@ -10,12 +11,16 @@ import ThemeProvider from '@/components/Theme';
 // components
 import PokestatsHead from '@/components/Head';
 import { AnimatePresence, motion } from 'framer-motion';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 // mui
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles';
 import theme from '@/MuiTheme';
 
 const App = ({ Component, pageProps, router }: AppProps): JSX.Element => {
+  // Create a client
+  const queryClient = new QueryClient();
+
   useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector('#jss-server-side');
@@ -26,23 +31,29 @@ const App = ({ Component, pageProps, router }: AppProps): JSX.Element => {
 
   return (
     <ThemeProvider>
-      <PokestatsHead />
-      <AnimatePresence mode="wait">
-        <motion.div
-          key={router.route}
-          initial="pageInitial"
-          animate="pageAnimate"
-          exit="pageExit"
-          variants={pageVariant}
-        >
-          <PlausibleProvider domain="pokestats.gg" enabled={process.env.NODE_ENV === 'production'}>
-            <MuiThemeProvider theme={theme}>
-              <CssBaseline />
-              <Component {...pageProps} />
-            </MuiThemeProvider>
-          </PlausibleProvider>
-        </motion.div>
-      </AnimatePresence>
+      <QueryClientProvider client={queryClient}>
+        <PokestatsHead />
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={router.route}
+            initial="pageInitial"
+            animate="pageAnimate"
+            exit="pageExit"
+            variants={pageVariant}
+          >
+            <PlausibleProvider
+              domain="pokestats.gg"
+              enabled={process.env.NODE_ENV === 'production'}
+            >
+              <MuiThemeProvider theme={theme}>
+                <CssBaseline />
+                <Component {...pageProps} />
+              </MuiThemeProvider>
+            </PlausibleProvider>
+          </motion.div>
+        </AnimatePresence>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 };
